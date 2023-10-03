fb2e4be60c34c0bef5091695cd6ca9c6a8ae6591
Jakub Nowakowski <jakubnowakowski98@gmail.com>
2023-10-03 14:39:11 +0200

    Initial Commit

---
+++ Swift_1_jn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift_1_jn
+//
+//  Created by student on 18/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift_1_jn/Swift_1_jnApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_1_jnApp.swift
+//  Swift_1_jn
+//
+//  Created by student on 18/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Swift_1_jnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
