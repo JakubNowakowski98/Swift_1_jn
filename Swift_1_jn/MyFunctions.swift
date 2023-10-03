f0e6c8e6f838253eed1eb2778f533d018c36b7ae
Jakub Nowakowski <jakubnowakowski98@gmail.com>
2023-10-03 15:13:31 +0200

    lab1

---
+++ Swift_1_jn/MyFunctions.swift
@@ -0,0 +1,8 @@
+//
+//  MyFunctions.swift
+//  Swift_1_jn
+//
+//  Created by student on 18/03/1445 AH.
+//
+
+import Foundation
---
+++ Swift_1_jn/MyTF.swift
@@ -0,0 +1,18 @@
+//
+//  MyTF.swift
+//  Swift_1_jn
+//
+//  Created by student on 18/03/1445 AH.
+//
+
+import SwiftUI
+
+struct MyTF: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyTF()
+}
